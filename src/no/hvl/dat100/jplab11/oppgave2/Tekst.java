560cfeb870a4af30568849007052a28aa8dc7a82
dcj152070 <111758889+dcj152070@users.noreply.github.com>
2022-11-07 15:09:30 +0100

    Update Tekst.java

--- src/no/hvl/dat100/jplab11/oppgave2/Tekst.java
+++ src/no/hvl/dat100/jplab11/oppgave2/Tekst.java
@@ -33,7 +33,7 @@ public class Tekst extends Innlegg {
 
 	@Override
 	public String toString() {
-		return "TEKST" + "\n" + super.toString() + tekst + "\n";
+		return "TEKST\n" + super.toString() + tekst + "\n";
 
 	}
 
