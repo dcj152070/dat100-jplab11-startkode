909e05b645056b7469e3535c2afab2d4b6042b38
dcj152070 <111758889+dcj152070@users.noreply.github.com>
2022-11-16 20:44:18 +0100

    Update SkrivBlogg.java

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -1,15 +1,32 @@
 package no.hvl.dat100.jplab11.oppgave4;
 
+// oppg4)
+
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
-import no.hvl.dat100.jplab11.common.TODO;
-import no.hvl.dat100.jplab11.oppgave3.*;
+import no.hvl.dat100.jplab11.oppgave3.Blogg;
 
 public class SkrivBlogg {
 
-	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
+	public static String MAPPE = System.getProperty
+			("user.dir") + 
+			"/src/no/hvl/dat100/jplab11/tests/";
+	
+	public static boolean skriv(Blogg samling, String mappe,  String filnavn) {
+		PrintWriter skriver;
+		try {		
+			skriver = new PrintWriter(MAPPE + filnavn);
+			
+		} catch (FileNotFoundException e) {
+			return true;
+			
+		}
+		String tekst = samling.toString();
+		skriver.print(tekst);
+		
+		skriver.close();
+		return false;
 	}
+	
 }
